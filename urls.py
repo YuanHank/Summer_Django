52e6d0c9f3a6d82286a67b11ac521343352c1959
YuanHank <110829997+YuanHank@users.noreply.github.com>
2022-08-19 17:32:02 +0800

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,30 @@
+"""Summer_Django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,re_path
+from new_app import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/',views.hello_world),
+    path('', views.index),
+    path('user/',views.user),
+    path('user/ajax_data/', views.ajax_data), #這個url 會導向ajax_data這個function
+    path('web_data/',views.web_data),
+    path('web_data/ajax/',views.web_data_ajax),
+    path('gene_list/',views.gene_list),
+    path('web_data/crawler/',views.crawler),
+    re_path(r'web_data/output/(?P<pk>.+)',views.output)
+]
---
+++ web_data.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    
+    $('#submit').click(function(){
+        
+        $.ajax({
+            headers: { 'X-CSRFToken': csrf_token },
+            type: 'POST',
+            url: 'ajax/', 
+            data: $('#ajax_form').serialize(),
+            success: function(response){
+                transcript = response.transcript.replace(/'/g,'');
+                transcript = transcript.replace('[','');
+                transcript = transcript.replace(']','');
+                transcript = transcript.replace(/,/g,"|")
+                //transcript = transcript.replace(/,/g,'|');
+                const transcript_list = transcript.split('|')
+                var len = transcript_list.length
+                $("#gene_id").empty();
+                $("#transcript").empty();
+                $("#numbers").empty();
+                //tr ='<td>'+response.Gene_ID+'</td>'+'<td>'+transcript+'</td>'+'<td>'+response.numbers+'</td>';
+                //$("#message").html('<div class="alert alert-warning">' + response.message + '</div>');
+                $("#gene_id").html(response.Gene_ID);
+                for (var i =0;i<len;i++){
+                    $("#transcript").append('<a href =web_data/output/'+transcript_list[i]+'>'+transcript_list[i]+"</a>");
+                } // by using loop , I can append transcipts seperatly and giving it a url,
+                //$("#transcript").html(transcript); //this line is old version which will only get the string of the transcript
+                $("#number").html(response.numbers);
+            },//回傳response
+            error: function(){
+                alert('Something error');
+            },
+        });
+
+    });
+    
+    //$('#transcript').on('click','#transcript_button',function(){
+    //    var fired_button = $(this).val();
+    //    window.location.href = 'output/';
+        
+    //    $.ajax({
+    //        headers: { 'X-CSRFToken': csrf_token },
+    //        type: 'POST',
+    //        url: 'crawler/', 
+    //        data: {'transcript':fired_button},
+    //        success: function(response){
+                
+    //            alert(response.name);
+    //            $("#output").html('<h2>'+response.name+'</h2>');
+    //        },
+    //        error: function(){
+    //            alert('Something error');
+    //        },
+        
+    //    });
+    //});
+});
+
+//https://wormbase.org/species/c_elegans/transcript/"+transcript_list[i]+"#06--10
+//'crawler/' id ="+transcript_list[i]+">"
