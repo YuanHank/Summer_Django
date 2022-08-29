876b808c778caa310e0565f2bc595c818d6ee7b0
YuanHank <110829997+YuanHank@users.noreply.github.com>
2022-08-29 15:03:23 +0800

    Update output.js

--- static/js/output.js
+++ static/js/output.js
@@ -13,4 +13,23 @@ $(document).ready(function(){
         $("#protein_table").toggle("slow");
         });        
 
+
+
+    $('#sequence_name').on('click',function(){
+        $('#Sequence').empty()
+        var fired_button = document.URL; //用這個可以得到現在的URL網址
+        transcript = fired_button.replace('http://127.0.0.1:8000/web_data/output/','')
+        $.ajax({
+            headers: { 'X-CSRFToken': csrf_token },
+            type: 'POST',
+            url: 'crawler/', 
+            data: {'transcript':transcript},
+            success: function(response){
+                $('#Sequence').append('<p>'+response.sequence_fin+'</p>')
+            },//回傳response
+            error: function(){
+                alert('Something error');
+            },
+        });
+    });
 });
