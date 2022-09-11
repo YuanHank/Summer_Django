555a64cd004563c12dd8d2b7065061cf71b99519
YuanHank <110829997+YuanHank@users.noreply.github.com>
2022-09-11 15:09:35 +0800

    Add files via upload

---
+++ Summer_Django/new_app/wormbase.py
@@ -0,0 +1,44 @@
+import urllib.request as request
+import bs4
+import json
+import pandas as pd
+#transcript = 'Y44E3A.2.1'
+def wormbase_crawler(transcript):
+    url = 'https://wormbase.org/rest/widget/transcript/'+transcript+'/sequences'
+
+    req = request.Request(url,headers ={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"})
+    with request.urlopen(req) as response:
+        sequence = response.read().decode("utf-8")
+    sequence  = json.loads(sequence)
+    #print (sequence)
+    #print('strand:',sequence['fields']['unspliced_sequence_context']['data']['strand'])#['positive_strand']['sequence'])
+    strand = sequence['fields']['unspliced_sequence_context']['data']['strand']
+    #print(sequence)
+    if strand =='+':
+        sequence_fin= sequence['fields']['unspliced_sequence_context']['data']['positive_strand']['sequence']
+        exon_intron = pd.DataFrame(sequence['fields']['unspliced_sequence_context']['data']['positive_strand']['features'])
+        exon_intron = exon_intron[['type','start','stop']]
+        exon = pd.DataFrame(sequence['fields']['spliced_sequence_context']['data']['positive_strand']['features'])
+        exon = exon[['type','start','stop']]
+        try:
+            protein = sequence['fields']['protein_sequence']['data']['sequence']
+        except:
+            protein = 'no such data'
+    elif strand =='-':
+        sequence_fin = sequence['fields']['unspliced_sequence_context']['data']['negative_strand']['sequence']
+        exon_intron = pd.DataFrame(sequence['fields']['unspliced_sequence_context']['data']['negative_strand']['features'])
+        exon_intron = exon_intron[['type','start','stop']]
+        exon = pd.DataFrame(sequence['fields']['spliced_sequence_context']['data']['negative_strand']['features'])
+        exon = exon[['type','start','stop']]
+        try:
+            protein = sequence['fields']['protein_sequence']['data']['sequence']
+        except:
+            protein = 'no such data'
+    return(sequence_fin,exon_intron,exon,protein)
+#print(exon)
+#print(protein)
+#Y44E3A.2.1 UTR -> intron ,not UTR -> EXON
+# background color #FFFFA3 ->Exon
+if __name__ =='__main__':
+    transcript = 'Y44E3A.2.1'
+    wormbase_crawler(transcript)
