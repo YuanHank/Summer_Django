1eb21358c6caa4ebc68933868fc3e959f70dfc70
YuanHank <110829997+YuanHank@users.noreply.github.com>
2022-09-12 00:51:12 +0800

    Update output.js

--- Summer_Django/static/js/output.js
+++ Summer_Django/static/js/output.js
@@ -17,8 +17,12 @@ $(document).ready(function(){
         });        
 
 
-
+    var start_end_type=[]
+    var tooltips 
+    var rects 
+    
     $('#sequence_name').on('click',function(){
+        $('.tooltip2').empty()
         $('#Sequence').empty()
         $('#title-num').empty()
         $('#rect-unspliced').empty()
@@ -55,20 +59,6 @@ $(document).ready(function(){
                 var color = 'orange';
                 var rect_color='orange';
                 var rect =''
-                var start_end_type=[]
-                //set axis
-                /*
-                for (var i=0;i<exon_intron_type.length;i++){
-                    start_end_type.push({x:exon_intron_start[i],y:exon_intron_stop[i],z:exon_intron_type[i]})
-                };
-                const xScale = d3.scaleLinear()
-                 .domain([0, d3.max(xData)])
-                 .range([10, 290]);
-                const xAxis = d3
-                 .axisBottom(xScale)
-                 
-                d3.select('.rect-unspliced').append("g").call(xAxis);
-                */
                 // set numbers
                 for (var i=0;i<title.length;i++){
                     $("#title-num").append(`<span class='g5'>${title[i]}</span><br>`)
@@ -112,74 +102,112 @@ $(document).ready(function(){
                         };
                     }
                 };   
-                // set rect pic
+                // set rect data dict/ where after '//' is the old way to set rect by for loop 
                 for (var i=0;i<exon_intron_type.length;i++){
                     if (exon_intron_type[i] == 'five_prime_UTR')
                     {
-                        rect =rect+ `<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:gray" id ="exon_intron_${i}" class="rect_exon_intron"/>`;
-                        //rect = rect+` <rect x="1" y="10" width="20" height="10" style ="fill:gray"/>`
+                        start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'gray',x:Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                        //rect =rect+ `<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:gray;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`;
+                        
                     }
                     else if (exon_intron_type[i] == 'exon') {
                         if (i==1){
                             if (rect_color == 'yellow'){
-                                rect += `<rect x="${(Number(exon_intron_stop[0])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_stop[i-1])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:orange" id ="exon_intron_${i}" class="rect_exon_intron"/>`;
+                                start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'orange',x:(Number(exon_intron_stop[0])+1)*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_stop[i-1])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                                //rect += `<rect x="${(Number(exon_intron_stop[0])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_stop[i-1])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:orange;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`;
                                 rect_color = 'orange'    
-                                //rect = rect+` <rect x="10" y="10" width="20" height="10" style ="fill:orange"/>`
+                                
                             }
                             else if (rect_color=='orange'){
-                                rect +=`<rect x="${(Number(exon_intron_stop[0])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_stop[i-1])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:yellow" id ="exon_intron_${i}" class="rect_exon_intron"/>`
-                                //rect = rect+` <rect x="10" y="10" width="20" height="10" style ="fill:yellow"/>`
+                                start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'yellow',x:(Number(exon_intron_stop[0])+1)*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_stop[i-1])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                                //rect +=`<rect x="${(Number(exon_intron_stop[0])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_stop[i-1])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:yellow;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`
+                                
                                 
                                 rect_color = 'yellow'
                             }
                         }
                         else if (i ==exon_intron_type.length-2){
                             if (rect_color == 'yellow'){
-                                rect += `<rect x="${(Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_start[i+1])-Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:orange" id ="exon_intron_${i}" class="rect_exon_intron" />`;
+                                start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'orange',x:(Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_start[i+1])-Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]})
+                                //rect += `<rect x="${(Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_start[i+1])-Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:orange;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron" />`;
                                 rect_color = 'orange'    
-                                //rect = rect+` <rect x="10" y="10" width="20" height="10" style ="fill:orange"/>`
+                                
                             }
                             else if (rect_color=='orange'){
-                                rect +=`<rect x="${((Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1])}" y="10" width="${(Number(exon_intron_start[i+1])-Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:yellow" id ="exon_intron_${i}" class="rect_exon_intron"/>`
-                                //rect = rect+` <rect x="10" y="10" width="20" height="10" style ="fill:yellow"/>`
-                                
+                                start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'yellow',x:((Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]),width:(Number(exon_intron_start[i+1])-Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]})
+                                //rect +=`<rect x="${((Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1])}" y="10" width="${(Number(exon_intron_start[i+1])-Number(exon_intron_start[i]))*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:yellow;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`
                                 rect_color = 'yellow'
                             }
                         }
                         else if(i!=1 & i!=exon_intron_type.length-2){
                             if (rect_color == 'yellow'){
-                                rect+=`<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:orange" id ="exon_intron_${i}" class="rect_exon_intron"/>`
+                                start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'orange',x:Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                               // rect+=`<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:orange;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`
                                 rect_color = 'orange';
                             }else if (rect_color=='orange'){
-                                rect+=`<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:yellow" id ="exon_intron_${i}" class="rect_exon_intron"/>`
+                                start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'yellow',x:Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                               // rect+=`<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:yellow;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`
                                 rect_color= 'yellow';  
                              }
                         };
                     } 
                     else if (exon_intron_type[i] == 'intron') {
-                        rect+=`<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:white" id ="exon_intron_${i}" class="rect_exon_intron"/>`
+                        start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'white',x:Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                       // rect+=`<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:white;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`
                      
                     }
                     else if (exon_intron_type[i] == 'three_prime_UTR') {
-                        rect += `<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:gray" id ="exon_intron_${i}" class="rect_exon_intron"/>`
+                        start_end_type.push({start:Number(exon_intron_start[i]),end:Number(exon_intron_stop[i]),type:String(exon_intron_type[i]),color:'gray',x:Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1],width:(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]})
+                        //rect += `<rect x="${Number(exon_intron_start[i])*100/exon_intron_stop[exon_intron_type.length-1]}" y="10" width="${(Number(exon_intron_stop[i])-Number(exon_intron_start[i])+1)*100/exon_intron_stop[exon_intron_type.length-1]}" height="2" style="fill:gray;cursor:pointer" id ="exon_intron_${i}" class="rect_exon_intron"/>`
                     }
                 };   
-                $('#rect-unspliced').append(`<svg viewbox="0,9.5,110,3" preserveAspectRatio="xMinYMin meet"> ${rect}</svg>`)
-                for (var i=0;i<exon_intron_type.length;i++){
-                    const tooltip = d3.select(`#exon_intron_${i}`)
-                    .append("div")
-                    .attr("id","rect_value")
-                    .style("position", "absolute")
-                    .style("visibility", "hidden") // 一開始tooltips是隱藏的
-                    .style("background-color", "white")
-                    .style("border", "solid")
-                    .style("border-width", "1px")
-                    .style("border-radius", "5px")
-                    .style("padding", "10px")
-                    .html(`<p>type:${exon_intron_type[i]}</p><p>start:${exon_intron_start[i]}</p>
-                        <p>end:${exon_intron_stop[i]}</p><p>length:${Number(exon_intron_stop[i])-Number(exon_intron_start[i])}</p>`
-                        );                
-                };
+                //$('#rect-unspliced').append(`<svg viewbox="0,9.5,110,3" preserveAspectRatio="xMinYMin meet"> ${rect}</svg>`) //the old way to append rect by loop
+
+                // by using d3 append rect and set hover by toolips
+                d3.select('#rect-unspliced').style('position', 'relative')
+                rects = d3.select('#rect-unspliced')
+                .append('svg')
+                .attr('viewBox',"0,9.5,110,3")
+                .attr('preserveAspectRation',"xMinYMin meet")
+                .selectAll('rect')
+                .data(start_end_type)
+                .enter()
+                .append('rect')
+                .attr('x', d => d.x)
+                .attr('width', d => d.width)
+                .attr('y',"10")
+                .attr('fill', d => d.color)
+                .attr('start',d =>d.start)
+                .attr('height',"2")
+                .style('cursor', 'pointer')
+                .attr("id",'rects');
+
+                tooltips = d3.select("#rect-unspliced")
+                .append("div")
+                .style("opacity", 0)
+                .style('position', 'absolute')
+                .attr("class", "tooltip")
+                .style("background-color", "white")
+                .style("border", "solid")
+                .style("border-width", "2px")
+                .style("border-radius", "5px")
+                .style("padding", "5px")
+                .attr('id','tooltips')
+            
+                d3.selectAll('#rects').on('mouseover', function(){
+                tooltips.style("opacity", 1) // 顯示tooltip
+                })
+                .on('mousemove', function(d){
+                let pt = d3.pointer(this) // 抓圓點位置
+                tooltips.style("opacity", 1)
+                .style('left', pt[0]+30+'px') // 設定tooltips位置
+                .style('top', pt[1]+'px')
+                .html(`Type:${d.target.__data__.type}<br>Start:${d.target.__data__.start}<br>End:${d.target.__data__.end}`) // 抓到綁定在DOM元素的資料                
+                })    
+                .on('mouseleave', function(){ //設定滑鼠離開時tooltips隱藏
+             tooltips.style("opacity", 0)  
+                });
+                            
             },
             //alert(rect)
             //回傳response
@@ -218,6 +246,35 @@ $(document).ready(function(){
         });
     });
 
+    //d3 test
 
+
+    /*
+    $('#rects').on('mouseover', function(){
+    tooltips.style("opacity", 1) // 顯示tooltip
+    })
+    .on('mousemove', function(d){
+    let pt = d3.pointer(event, this) // 抓圓點位置
+    $('#tooltips').style("opacity", 1)
+    .style('left', pt[0]+30+'px') // 設定tooltips位置
+    .style('top', pt[1]+'px')
+    .html(`start${d.target._data_.start}`) // 抓到綁定在DOM元素的資料
+    })   */   
+
+    
+    /*
+    //
+    const tooltip = d3.select(`#rect-unspliced`)
+    .append("div")
+    .style("position", "absolute")
+    .style("visibility", "visible") // 一開始tooltips是隱藏的
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .html(`<p>type:${exon_intron_type[i]}</p><p>start:${exon_intron_start[i]}</p>
+        <p>end:${exon_intron_stop[i]}</p><p>length:${Number(exon_intron_stop[i])-Number(exon_intron_start[i])}</p>`
+        ); */
 });
 
